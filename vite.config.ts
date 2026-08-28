b104b83a64a25932920f4785e68f5359eeb8c7ec
C-Vac <17118143+C-Vac@users.noreply.github.com>
2026-08-28 17:06:42 -0400

    refactor: simplify vite config and build to root dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/games/chess/',
-  build: {
-    outDir: 'dist/games/chess'
-  }
+  plugins: [react()]
 })
